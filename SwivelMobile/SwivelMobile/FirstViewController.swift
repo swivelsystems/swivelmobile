1c8779d9defa6d4e922163a09fc42df4a0b1684d
Jonathen Chen <jonathen0915@gmail.com>
2016-05-03 09:48:58 -0700

    feat/ Create project template, view controller, setup icons, and create load page

---
+++ SwivelMobile/SwivelMobile/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SwivelMobile
+//
+//  Created by Jonathen Chen on 2016-04-30.
+//  Copyright © 2016 Swivel Systems. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
