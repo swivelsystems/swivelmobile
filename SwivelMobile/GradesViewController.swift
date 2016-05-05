d7cbe3f38ec9efea4d2a632ffdacafd76d0fa686
Jonathen Chen <jonathen0915@gmail.com>
2016-05-04 18:51:04 -0700

    Complete GradeView format and rendering with dummy data and placeholder images

--- SwivelMobile/GradesViewController.swift
+++ SwivelMobile/GradesViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class GradesViewController: UIViewController {
+class GradesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    //Table Rendering
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,8 +22,24 @@ class GradesViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
+
+    //Table Rendering functions
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! GradesCell
+          cell.course?.text = "SOME COURSE"
+          cell.teacher?.text = "MRS. Vickies"
+          cell.average?.text = "75%"
+          cell.grade?.text = "84%"
+//            cell.course?.text = "Computer Science 101"
+//            cell.teacher?.text = "Mrs. AppleBees"
+//            //cell.studentAverage?.text = "75%"
+//            cell.grade?.text = "87%"
+        return cell
+    }
 
 
 }
