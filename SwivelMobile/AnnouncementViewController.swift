d59a2d061104539932d9fa2c428c33602b0a4cb1
Jonathen Chen <jonathen0915@gmail.com>
2016-05-06 10:08:55 -0700

    Feat / complete grades and due dates view with styling and dummy data placeholder (#8)
    feat / Complete grades and due dates view with styling and dummy data placeholder (resolves #8)

---
+++ SwivelMobile/AnnouncementViewController.swift
@@ -0,0 +1,71 @@
+//
+//  FirstViewController.swift
+//  SwivelMobile
+//
+//  Created by Jonathen Chen on 2016-04-30.
+//  Copyright © 2016 Swivel Systems. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import CoreData
+
+
+class AnnouncementViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    //Table Rendering
+    @IBOutlet weak var tableView: UITableView!
+    
+    var studentId = "1234"
+
+    var students: JSON?
+
+    func table_refresh() {
+        dispatch_async(dispatch_get_main_queue(), {
+            self.tableView.reloadData()
+            return
+        })
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(Package.sharedInstance.data)
+        let refreshControl = UIRefreshControl()
+        tableView.dataSource = self
+        refreshControl.addTarget(self, action: "refreshAnnouncements:", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.addSubview(refreshControl)
+
+//        Alamofire.request(.GET,"http://localhost:3000/students/:" + studentId)
+//            .response { (request, response, data, error) in
+//                struct package {
+//                    
+//                }
+//                self.students = JSON(data: data!)
+//                self.table_refresh()
+//        }
+    }
+    
+    func refreshAnnouncements(refreshControl: UIRefreshControl) {
+        print("refresh announcements")
+        refreshControl.endRefreshing()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! AnnouncementCell
+        cell.photo.image = UIImage(named:"Haomeme")
+        //cell.name?.text = students?[indexPath.row]["name"]["first"].string!
+        //cell.email?.text = students?[indexPath.row]["email"].string!
+        return cell
+    }
+
+}
+
---
+++ SwivelMobile/GradesCell.swift
@@ -0,0 +1,16 @@
+//
+//  GradesCell.swift
+//  SwivelMobile
+//
+//  Created by Jonathen Chen on 2016-05-04.
+//  Copyright © 2016 Swivel Systems. All rights reserved.
+//
+
+import UIKit
+
+class GradesCell: UITableViewCell {
+    @IBOutlet weak var course: UILabel!
+    @IBOutlet weak var teacher: UILabel!
+    @IBOutlet weak var average: UILabel!
+    @IBOutlet weak var grade: UILabel!
+}
