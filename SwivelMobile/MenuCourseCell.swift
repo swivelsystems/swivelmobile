617bd6d34830289fe56c755b31a9f5dcf6b08bd2
Jonathen Chen <jonathen0915@gmail.com>
2016-05-10 12:36:12 -0700

    feat / Revamp menu sideBar for style and functionality
    Completely redesigned the menu sideBar following a similar style to the twitch
    ios app. Encountered issue where a tableView controller could not support both
    dynamic and static. Solved issue by separating the static cell from the menu
    into a seperate container. Added categories and profile/settings icon at the bottom
    of the sidebar and formatted to look consistent across different devices
    
    Commits:
    - Create basic templating for new menu (added image location, title, dynamic table)
    - Separated static cells from dynamic cells using container
    - Added new cell class and functionality for dynamic tableView
    - Added image assets and lower Profile and settings button
    - Complete final touches on menu styling

---
+++ SwivelMobile/MenuCourseCell.swift
@@ -0,0 +1,15 @@
+//
+//  MenuCourseCell.swift
+//  SwivelMobile
+//
+//  Created by Jonathen Chen on 2016-05-10.
+//  Copyright © 2016 Swivel Systems. All rights reserved.
+//
+
+import UIKit
+
+class MenuCourseCell: UITableViewCell {
+
+    @IBOutlet weak var courseIcon: UIImageView!
+    @IBOutlet weak var courseTitle: UILabel!
+}
